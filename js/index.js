5bf7a1cf714c75aed53f3cc04d6f295144c9ba83
GianinaSoloa <gianina.soloa@hotmail.com>
2022-05-12 21:05:09 -0300

    Desafío: Incorporar eventos

--- js/index.js
+++ js/index.js
@@ -1,16 +1,15 @@
 
 /* La declaro como let ya que probablemente se modifique en el tiempo debido a la inflación */
-
 let usd = 800;
 
 class Abertura {
   constructor (tipoAbertura, porcentaje) {
     this.tipoAbertura = tipoAbertura;
     this.porcentaje = porcentaje;
-    }
+  }
 
   calcularAbertura(){
-    return this.porcentaje * usd;
+    return this.porcentaje * usd; 
   }
 }
 
@@ -21,87 +20,79 @@ class Placard {
   }
 
   calcularPlacard(){
-	  return this.porcentaje * usd;
-  }
+    return this.porcentaje * usd;
+    }
 }
 
 const aberturas = []
-aberturas.push(new Abertura("aluminio", 0.05))
-aberturas.push(new Abertura("pvc", 0.075))
+aberturas.push(new Abertura("aluminio", 0.05));
+aberturas.push(new Abertura("pvc", 0.075));
 
 const placards = []
-placards.push(new Placard("sin placard", 0))
-placards.push(new Placard("melamina", 0.05))
-placards.push(new Placard("madera maciza", 0.075))
+placards.push(new Placard("sin placard", 0));
+placards.push(new Placard("melamina", 0.05));
+placards.push(new Placard("madera maciza", 0.075));
 
-/* Declaro 3 funciones ya que quiero que tenga siempre la posibilidad de salir en cada pregunta, y a futuro serán muchas más preguntas */
+const abertura = document.getElementById ("seleccionAbertura");
+aberturas.forEach(el => {
+  abertura.innerHTML += `<option value ="${el.tipoAbertura}">${el.tipoAbertura}</option>`
+})
 
-const ingresoAbertura = () => {
-let aberturaCliente = prompt ("¿Qué aberturas prefieres? ¿aluminio ó pvc? Ingrese 0 si desea salir");
+const placard = document.getElementById ("seleccionPlacard");
+placards.forEach(el => {
+  placard.innerHTML += `<option value ="${el.tipoPlacard}">${el.tipoPlacard}</option>`
+})
 
-while (aberturaCliente !== "0" && aberturaCliente !== "aluminio" && aberturaCliente !== "pvc") {
-  alert("Por favor ingrese un parámetro correcto");
-  aberturaCliente = prompt ("¿Qué aberturas prefieres? ¿aluminio ó pvc? Ingrese 0 si desea salir");
-}
-
-if (aberturaCliente === "0"){ 
-  let botonPresupuesto = document.getElementById("botonPresupuesto");
-  botonPresupuesto.className = "background-color-error";
-  let parrafo = document.createElement("p");
-	parrafo.id = "pPresupuesto";
-	parrafo.innerHTML = "<p>Eligió salir. Para calcular un presupuesto haga clic nuevamente en el botón.</p>"; 
-	parrafoPresupuesto.append(parrafo);
-}
-  return aberturaCliente;
-}
-
-const ingresoPlacard = () => {
-let placardCliente = prompt ("¿Qué placard prefieres? ¿melamina, madera maciza o sin placard? Ingrese 0 si desea salir");
-while (placardCliente !== "0" && placardCliente !== "melamina" && placardCliente !== "madera maciza" && placardCliente !== "sin placard") {
-  alert("Por favor ingrese un parámetro correcto");
-  placardCliente = prompt ("¿Qué placard prefieres? ¿melamina, madera maciza o sin placard? Ingrese 0 si desea salir");
-}
-return placardCliente;
-}
-
-const ingresoMetros = () => {
-let metrosCliente = prompt ("¿Cuántos m2 tendrá tu casa? Ingrese 0 si desea salir");
-while (metrosCliente !== "0" && isNaN(metrosCliente)) {
-  alert("Por favor ingrese un número");
-  metrosCliente = parseFloat(prompt("¿Cuántos m2 tendrá tu casa? Ingrese 0 si desea salir"));
-}
-return metrosCliente;
-}
+const metrosMal = document.getElementById ("metros");
+metrosMal.addEventListener("input", () => {
+  if (isNaN(metrosMal.value)) {
+    let error = document.createElement("div");
+    error.className = "container";
+    error.innerHTML = "<h3>Por favor ingrese un número</h3>"; 
+    calcularPresupuesto.append(error);
+  
+    let botonCerrarError = document.createElement("a");
+    botonCerrarError.className = "botonPresupuesto";
+    botonCerrarError.innerHTML = "<a>CERRAR</a>"; 
+    error.append(botonCerrarError);
+  
+    
+    botonCerrarError.onclick = () => {
+      error.className = "containerNotShow";
+    }
+    
+    metrosMal.className = "error";
+  }
+})
 
 let boton = document.getElementById("botonPresupuesto")
 boton.onclick = () => {
+  
 /* Comienzo cálculo de presupuesto */
-let abertura = ingresoAbertura();
-  if (abertura !== "0"){
-    let placard = ingresoPlacard();
-      if (placard !== "0"){
-        let metros = ingresoMetros();
-          if (metros !== "0"){
-            const aberturaSeleccionada = aberturas.find((x) => x.tipoAbertura === abertura);
-            const placardSeleccionado = placards.find((x) => x.tipoPlacard === placard);
-            let seleccion =  aberturaSeleccionada.calcularAbertura() +  placardSeleccionado.calcularPlacard() + usd;
-            const total = (seleccion,metros) => seleccion * metros;
-            let casaTotal = total(seleccion,metros);      
-            alert(`El precio será de USD ${seleccion}  por m2, por ende tu casa saldrá aproximadamente USD ${casaTotal}`);
-            
-            let botonPresupuesto = document.getElementById("botonPresupuesto");
-            botonPresupuesto.innerText = "Presupuesto Calculado Correctamente";
-            botonPresupuesto.className = "background-color-fine";
-          } else{
-            let botonPresupuesto = document.getElementById("botonPresupuesto");
-            botonPresupuesto.innerText = "Calcular Presupuesto";
-            }
-      } else{
-        let botonPresupuesto = document.getElementById("botonPresupuesto");
-        botonPresupuesto.innerText = "Calcular Presupuesto";	  
-        }
-  } else{
-    let botonPresupuesto = document.getElementById("botonPresupuesto");
-    botonPresupuesto.innerText = "Calcular Presupuesto";
-    }
+  const aberturaSeleccionada = aberturas.find((x) => x.tipoAbertura === abertura.value);
+  const placardSeleccionado = placards.find((x) => x.tipoPlacard === placard.value);
+  let seleccion =  aberturaSeleccionada.calcularAbertura() +  placardSeleccionado.calcularPlacard() + usd;
+  const total = (seleccion,metros) => seleccion * metros.value;
+  let casaTotal = total(seleccion,metros);  
+  
+  let presupuestoCreado = document.createElement("div");
+  presupuestoCreado.className = "container";
+  presupuestoCreado.innerHTML = `<h3>¡Tu presupuesto ha sido creado con éxito!</h3><h4>El precio será de USD ${seleccion} por m2, por ende tu casa saldrá aproximadamente USD ${casaTotal}</h4><br><br>`; 
+  calcularPresupuesto.append(presupuestoCreado);
+
+  let botonCerrar = document.createElement("a");
+  botonCerrar.className = "botonPresupuesto";
+  botonCerrar.innerHTML = "<a>CERRAR</a>"; 
+  presupuestoCreado.append(botonCerrar);
+
+  botonCerrar.onclick = () => {
+    presupuestoCreado.className = "containerNotShow";
+  }
+
+  const metrosBien = document.getElementById ("metros");
+  metrosBien.className = "correcto";  
+
 }
+
+
+
